edd57a59c6340f60723edc89d0018b00d6cbcc9c
Carlo Colombo <carlo.colombo@gmx.com>
2011-05-18 00:01:16 +0200

    added shell examples

---
+++ examples/copy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python /path/to/rensub.py $1 $2 $3 copy /media/player
+#copy both file to path /media/player
---
+++ examples/vlc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python /path/to/rensub.py $1 $2 $3 'vlc -f'
+#launch vlc fullscreen
