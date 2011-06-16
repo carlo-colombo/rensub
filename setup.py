21bee19d5dc0e0dced2a89e8686251af09f148e0
Carlo Colombo <carlo.colombo@gmx.com>
2011-06-17 00:54:22 +0200

    Installation scripts

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(name='Rensub',
+    version='1.0',
+    packages=['rensub'],
+    description='Subtitles utils',
+    author='Carlo Colombo',
+    author_email='carlo.colombo@gmx.com',
+    url='https://github.com/carlo-colombo/rensub',
+    scripts=['scripts/rensub']
+    )
