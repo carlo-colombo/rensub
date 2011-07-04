b5e3260cdce0711e079ef46709dac7eda616b3a6
Carlo Colombo <carlo.colombo@gmx.com>
2011-07-04 23:12:12 +0200

    clean up

--- rensub/eml.py
+++ rensub/eml.py
@@ -12,10 +12,6 @@ def rename(avi,srt):
     os.rename(srt,temp)
     try:
         shutil.move(temp,avidir)
-    except shutil.Error:
-        print "Subtitle file already exists, not overwritten"
-    except OSError:
-        pass
     finally:	  
         return temp
 
@@ -25,7 +21,6 @@ def unzip(zipped):
     purge directory and non srt file'''
     z=zipfile.ZipFile(zipped)
     zipdir=os.path.dirname(zipped)
-    #print z.namelist()
     files=[name for name in z.namelist() if not name.endswith(os.sep) and not name.startswith("__MAC") and name.endswith(".srt")]
     for name in files:
         out=open(os.path.join(zipdir,name),"wb")
@@ -48,29 +43,6 @@ def makeAll(avi,zipped):
 			r=0
     
     return rename(avi,files[r])
-    
-def main(argv):
-    if len(argv)>=3:
-          avi=argv[1]
-          zipped=argv[2]
-          srt=makeAll(avi,zipped)
-          if srt:
-              print avi,srt
-              if len(argv)==4:
-                  print os.popen(argv[3]+" "+avi)
-              if len(argv)==5 and argv[3]=="copy":
-                  shutil.copy(avi,argv[4])
-                  srtpath=os.path.split(avi)[0]+os.sep+os.path.split(srt)[1]
-                  shutil.copy(srtpath,argv[4])
-                  print "Copied"
-          else:
-              print "Error: multiple file in zip."
-          
-    else:
-          print '''Use: python renSubs.py <full path avi file> <full path zip file>'''
-
-if __name__ == '__main__':
-  main(sys.argv)
 
     
     
